696d9085b583f0ef19d2f6b9f47f24583c2bbd13
Mohammed Madani Ibrahim Ali Khan <165456579+Md-Madani@users.noreply.github.com>
2024-11-29 17:37:12 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,9 @@
+# Check if a string is palindrome
+def is_palindrome(s):
+    return s == s[::-1]
+
+word = input("Enter a word: ")
+if is_palindrome(word):
+    print(f"{word} is a palindrome")
+else:
+    print(f"{word} is not a palindrome")
